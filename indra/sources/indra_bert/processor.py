8b35840485b31c7247353c7c8e287df3dd563a91
Ben Gyori <ben.gyori@gmail.com>
2025-05-20 13:44:56 -0400

    Fix annotation ordering

--- indra/sources/indra_bert/processor.py
+++ indra/sources/indra_bert/processor.py
@@ -1,6 +1,7 @@
 from indra.statements import *
 from indra.ontology.standardize import standardize_agent_name
 
+
 class IndraBertProcessor:
     def __init__(self, data, grounder=None):
         self.data = data
@@ -15,7 +16,6 @@ class IndraBertProcessor:
         db_refs['TEXT'] = name
         agent = Agent(name, db_refs=db_refs)
         standardize_agent_name(agent, standardize_refs=True)
-        # TODO: grounding
         return agent
 
     def extract_statement(self, entry):
@@ -23,41 +23,60 @@ class IndraBertProcessor:
         roles = entry['role_pred']['roles']
         text = entry['original_text']
 
-        raw_texts = []
-        coords = []
-
         agents_by_role = {}
+        raw_texts = {}
+        coords = {}
         for agent_info in roles:
-            agents_by_role[agent_info['role']] = \
-                self.get_agent(agent_info, text)
-            raw_texts.append(agent_info['text'])
-            coords.append([agent_info['start'], agent_info['end']])
+            role = agent_info['role']
+            agents_by_role[role] = self.get_agent(agent_info, text)
+            raw_texts[role] = agent_info['text']
+            coords[role] = ([agent_info['start'], agent_info['end']])
 
         evidence = Evidence(
             source_api=self.source_api,
             text=text,
-            annotations={
+        )
+
+        stmt_class = get_statement_by_name(stmt_type)
+        if issubclass(stmt_class, Complex):
+            members = [agent for role, agent in agents_by_role.items()]
+            raw_texts = [raw_text for role, raw_text in raw_texts.items()]
+            coords = [coord for role, coord in coords.items()]
+            annotations = {
                 'agents': {
                     'raw_text': raw_texts,
                     'coords': coords
                 }
             }
-        )
-
-        stmt_class = get_statement_by_name(stmt_type)
-        if issubclass(stmt_class, Complex):
-            members = [agent for role, agent in agents_by_role.items()
-                       if role.startswith('member')]
+            evidence.annotations = annotations
             stmt = Complex(members, evidence=[evidence])
             return stmt
         elif issubclass(stmt_class, (RegulateAmount, RegulateActivity)):
             subj = agents_by_role.get('subject')
             obj = agents_by_role.get('object')
+            raw_texts = [raw_texts.get('subject'), raw_texts.get('object')]
+            coords = [coords.get('subject'), coords.get('object')]
+            annotations = {
+                'agents': {
+                    'raw_text': raw_texts,
+                    'coords': coords
+                }
+            }
+            evidence.annotations = annotations
             stmt = stmt_class(subj, obj, evidence=[evidence])
             return stmt
         elif issubclass(stmt_class, Modification):
             enz = agents_by_role.get('enz')
             sub = agents_by_role.get('sub')
+            raw_texts = [raw_texts.get('enz'), raw_texts.get('sub')]
+            coords = [coords.get('enz'), coords.get('sub')]
+            annotations = {
+                'agents': {
+                    'raw_text': raw_texts,
+                    'coords': coords
+                }
+            }
+            evidence.annotations = annotations
             stmt = stmt_class(enz, sub, evidence=[evidence])
             return stmt
         else:
